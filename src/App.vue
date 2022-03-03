435644bad5af35f124a92282baa76cb53354dbdc
GeovaniLima <geovani.lima.silva@hotmail.com>
2022-03-02 23:22:28 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+      <v-card>
+        <v-navigation-drawer
+          app
+          v-model="drawer"
+          :mini-variant.sync="mini"
+          permanent
+        >
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://avatars.githubusercontent.com/u/66522839?v=4"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-title>Geovani Lima</v-list-item-title>
+
+            <v-btn
+              icon
+              @click.stop="mini = !mini"
+            >
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list dense>
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+
+    <v-app-bar
+      app
+      color="#00335F"
+      dark
+    >
+      <v-app-bar-title>HAP Engenharia</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
+  export default {
+    name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'Minha Conta', icon: 'mdi-account' },
+          { title: 'Usuários', icon: 'mdi-account-group-outline' },
+        ],
+        mini: true,
+      }
+    },
+  };
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,193 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div> 
+
+    <v-container>
+      <h3>Editor de Modelo</h3>
+
+      <br>
+
+      <v-row
+
+        justify="space-between"
+      >
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-form ref="form">
+            <v-select
+              :items="items"
+              label="Dual Carriageway Rail"
+              outlined
+            ></v-select>
+
+            <div class="group_radios">
+              <div class="vista_movimento">
+                <p> Vista de Movimento</p>
+                <v-radio-group
+                  v-model="radios"
+                  mandatory
+                >
+                  <v-radio
+                    label="Esquerda"
+                    value="esquerda"
+                    color="#00335F"
+                  ></v-radio>
+                  <v-radio
+                    label="Direita"
+                    value="direita"
+                    color="#00335F"
+                  ></v-radio>
+                  <v-radio
+                    label="Ambos"
+                    value="ambos"
+                    color="#00335F"
+                  ></v-radio>
+                </v-radio-group>
+              </div>
+
+              <div class="ver">
+                <p> Ver </p>
+                <v-radio-group
+                  v-model="radios"
+                  mandatory
+                >
+                  <v-radio
+                    label="Cortar"
+                    value="cortar"
+                    color="#00335F"
+                  ></v-radio>
+                  <v-radio
+                    label="Preencher"
+                    value="preencher"
+                    color="#00335F"
+                  ></v-radio>
+                </v-radio-group>
+
+                <v-select
+                  :items="indice"
+                  label="Índice V:H"
+                  outlined
+                ></v-select>
+              </div>
+            </div>
+          </v-form>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <h2>Trilho</h2>
+
+          <p>Balastro</p>
+          <v-text-field
+            v-model="largura"
+            label="Largura"
+            suffix="m2"
+            color="#00335F"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="altura"
+            label="Altura"
+            suffix="m2"
+            color="#00335F"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="declive"
+            label="Declive"
+            suffix="(%)"
+            color="#00335F"
+            outlined
+          ></v-text-field>
+          <v-checkbox
+            v-model="superelevacao"
+            label="Super Elevação"
+            color="#00335F"
+          ></v-checkbox>
+
+          <p>Rastrear</p>
+          <v-text-field
+            v-model="medidor"
+            label="Medidor 1/2"
+            suffix="mm"
+            color="#00335F"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <div class="contaneir-img">
+
+        <v-checkbox
+          v-model="zoomajuste"
+          label="Zoom Ajuste"
+          color="#00335F"
+        ></v-checkbox>
+        <v-img
+          height="300"
+          src="https://www.tecnolegis.com/media/imagens/provas-fev-12/2012/metro/metrosp-metroviarios-agrimensura-q-35e36.png"
+        ></v-img>  
+      </div> 
+
+      <div class="container-buttons">
+        <v-btn class="btn-ok">
+          Ok
+        </v-btn>
+        <v-btn color="error">
+          Cancelar
+        </v-btn> 
+      </div>
+    </v-container>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    items: [
+      'Median_1', 
+      'Shoulder_1', 
+      'Rail_1', 
+      'Shoulder_2',
+      'Subgrade_1',
+      'Subgrade_2',
+      'Ditch_1'
+    ],
+    indice: [
+      '1:1'
+    ],
+  }),
 }
 </script>
+
+<style>
+  .group_radios {
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .contaneir-img {
+    margin-bottom: 50px;
+  }
+
+  .container-buttons {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .container-buttons .btn-ok {
+    margin-right: 5px;
+
+    background-color: #00335F !important;
+    color: #FFF;
+  }
+</style>
